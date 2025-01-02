09347fa89b34512b846a95f759f9d8a15e359867
MidhunViknCodes <midhunvikncodes@gmail.com>
2025-01-02 17:48:15 +0530

    shimmer loader added

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -11,6 +11,6 @@ _flutter.buildConfig = {"engineRevision":"cb4b5fff73850b2e42bd4de7cb9a4310a78ac4
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "1730908771"
+    serviceWorkerVersion: "3631114900"
   }
 });
