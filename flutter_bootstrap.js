f99c2abb5a11a5cddf2dcb75411afd2ff7c8bbad
MidhunViknCodes <midhunvikncodes@gmail.com>
2025-01-16 12:27:14 +0530

    dd

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -11,6 +11,6 @@ _flutter.buildConfig = {"engineRevision":"cb4b5fff73850b2e42bd4de7cb9a4310a78ac4
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "1458337467"
+    serviceWorkerVersion: "2370058027"
   }
 });
